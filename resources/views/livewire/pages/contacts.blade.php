6d04adc143f23aaf3d3ae028beaf62144fadec6f
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2023-11-11 23:09:35 +0100

    delete files

--- resources/views/livewire/pages/contacts.blade.php
+++ resources/views/livewire/pages/contacts.blade.php
@@ -9,37 +9,44 @@
             <a href="{{ route('events.create') }}" class="ml-8 button--classic">Créer un nouveau contact</a>
         </div>
         {{-- Liste des contacts --}}
-        <div class="contacts__list">
-            <h3>Liste des utilisateurs</h3>
-            <div class="events__users__list">
-                <div class="events__users__list__student">
-                    <h4 class="text-2xl font-bold my-6">Étudiants</h4>
+        <div class="grid grid-cols-2 gap-8">
+            <div class="col-span-1">
+                <div x-data="{ search: '' }">
+                    <h3 class="text-2xl font-bold mt-8 mb-4">Liste des étudiants</h3>
+                    <input type="text" x-model="search" placeholder="Rechercher un étudiant..." class="w-full mb-6 p-2 border border-gray-400 rounded-lg">
                     <ul>
                         @if(count($students) > 0)
                             @foreach($students as $student)
-                                <li class="bg-gray-100 rounded-lg p-4 mb-4">
-                                    <div class="font-bold">{{ $student->name }}</div>
-                                    <div class="text-gray-600">{{ $student->email }}</div>
-                                    <div class="text-gray-600">id : {{ $student->id }}</div>
-                                    <div class="text-gray-600">user-id : {{ $student->user_id }}</div>
-                                </li>
+                                <template x-if="search === '' || '{{ $student->name }}'.toLowerCase().includes(search.toLowerCase())">
+                                    <li class="bg-gray-100 rounded-lg p-4 mb-4">
+                                        <div class="font-bold">{{ $student->name }}</div>
+                                        <div class="text-gray-600">{{ $student->email }}</div>
+                                        <div class="text-gray-600">id : {{ $student->id }}</div>
+                                        <div class="text-gray-600">user-id : {{ $student->user_id }}</div>
+                                    </li>
+                                </template>
                             @endforeach
                         @else
                             <p>Aucun étudiant à afficher pour le moment.</p>
                         @endif
                     </ul>
                 </div>
-                <div class="events__users__list__evaluator">
-                    <h4 class="text-2xl font-bold my-6">Évaluateurs</h4>
+            </div>
+            <div class="col-span-1">
+                <div x-data="{ search: '' }">
+                    <h3 class="text-2xl font-bold mt-8 mb-4">Liste des évaluateurs</h3>
+                    <input type="text" x-model="search" placeholder="Rechercher un évaluateur..." class="w-full mb-4 p-2 border border-gray-400 rounded-lg">
                     <ul>
-                    @if(count($evaluators) > 0)
+                        @if(count($evaluators) > 0)
                             @foreach($evaluators as $evaluator)
-                                <li class="bg-gray-100 rounded-lg p-4 mb-4">
-                                    <div class="font-bold">{{ $evaluator->name }}</div>
-                                    <div class="text-gray-600">{{ $evaluator->email }}</div>
-                                    <div class="text-gray-600">id : {{ $evaluator->id }}</div>
-                                    <div class="text-gray-600">user-id : {{ $evaluator->user_id }}</div>
-                                </li>
+                                <template x-if="search === '' || '{{ $evaluator->name }}'.toLowerCase().includes(search.toLowerCase())">
+                                    <li class="bg-gray-100 rounded-lg p-4 mb-4">
+                                        <div class="font-bold">{{ $evaluator->name }}</div>
+                                        <div class="text-gray-600">{{ $evaluator->email }}</div>
+                                        <div class="text-gray-600">id : {{ $evaluator->id }}</div>
+                                        <div class="text-gray-600">user-id : {{ $evaluator->user_id }}</div>
+                                    </li>
+                                </template>
                             @endforeach
                         @else
                             <p>Aucun membre du jury à afficher pour le moment.</p>
--- resources/views/livewire/pages/projects.blade.php
+++ resources/views/livewire/pages/projects.blade.php
@@ -8,9 +8,50 @@
             />
             <a href="{{ route('events.create') }}" class="ml-8 button--classic">Créer un nouveau projet</a>
         </div>
-        {{-- Liste des projets --}}
-        <div class="projects__list">
 
+        {{-- Display all the projects --}}
+        <table class="table-auto my-12">
+            <thead>
+                <tr>
+                    <th class="px-4 py-2">#</th>
+                    <th class="px-4 py-2">Nom</th>
+                    <th class="px-4 py-2">Description</th>
+                    <th class="px-4 py-2">Category</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($projects as $index => $project)
+                    <tr>
+                        <td class="border px-4 py-2">{{ $index + 1 }}</td>
+                        <td class="border px-4 py-2">{{ ucfirst($project->name) }}</td>
+                        <td class="border px-4 py-2">{{ $project->description }}</td>
+                        <td class="border px-4 py-2">{{ ucfirst($project->category) }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        {{-- Display all the projects --}}
+        <div class="projects__list">
+            @foreach($events->sortBy('starting_at') as $event)
+                <h2 class="text-2xl font-bold mb-4">
+                    {{ $event->name }}
+                    {{-- Display the event's Date --}}
+                    <span class="text-gray-600 text-sm ml-2">({{ $event->starting_at }})</span>
+                    {{-- Display the event's Duration --}}
+                    <span class="text-gray-600 text-sm ml-2">({{ $event->duration }} min)</span>
+                </h2>
+                <ul class="list-disc pl-8">
+                    @foreach($projects->sortBy('name') as $project)
+                        <li class="mb-4 border-b border-gray-200 pb-4">
+                            Projet {{ $loop->iteration }}
+                            <h3 class="text-lg font-bold leading-tight mb-2"><strong>Nom:</strong> {{ $project->name }}</h3>
+                            <p class="text-gray-600 leading-tight mb-2"><strong>Description:</strong> {{ $project->description }}</p>
+                            <p class="text-gray-600 leading-tight mb-2"><strong>Category:</strong> {{ $project->category }}</p>
+                        </li>
+                    @endforeach
+                </ul>
+            @endforeach
         </div>
     </main>
 </x-app-layout>
