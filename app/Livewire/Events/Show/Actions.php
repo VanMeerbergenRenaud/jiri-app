22b5a57a89bc365755c48a3b63f6c2b408aa38aa
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2024-07-28 16:42:01 +0200

    Feature done to update the status of an event on the dashboard

--- app/Livewire/Events/Show/Actions.php
+++ app/Livewire/Events/Show/Actions.php
@@ -2,32 +2,68 @@
 
 namespace App\Livewire\Events\Show;
 
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Actions extends Component
 {
     public $event;
+
     public $hours = '00';
+
     public $minutes = '00';
+
     public $seconds = '00';
+
+    public $eventOnPause = true;
 
     public function mount($event)
     {
         $this->event = $event;
-        $this->hours = '00';
-        $this->minutes = '00';
-        $this->seconds = '00';
+
         $this->updateTimer();
     }
 
     public function updateTimer()
     {
-        $now = now();
-        $startedAt = $this->event->started_at;
-        $diff = $now->diff($startedAt);
+        $now = Carbon::now();
+        $startedAt = Carbon::parse($this->event->started_at);
+        $finishedAt = Carbon::parse($this->event->finished_at);
+
+        if ($finishedAt !== null) {
+            $diff = $finishedAt->diff($startedAt);
+        } else {
+            $diff = $now->diff($startedAt);
+        }
+
         $this->hours = str_pad($diff->h, 2, '0', STR_PAD_LEFT);
         $this->minutes = str_pad($diff->i, 2, '0', STR_PAD_LEFT);
         $this->seconds = str_pad($diff->s, 2, '0', STR_PAD_LEFT);
+    }
+
+    public function pauseEvent()
+    {
+        $this->event->update([
+            'paused_at' => now(),
+        ]);
+    }
+
+    public function continueEvent()
+    {
+        $this->event->update([
+            'paused_at' => null,
+        ]);
+
+        $this->reset('eventOnPause');
+    }
+
+    public function finishEvent()
+    {
+        $this->event->update([
+            'finished_at' => now(),
+        ]);
+
+        return redirect()->route('events.index');
     }
 
     public function updateStatus($status)
--- resources/views/livewire/events/show/actions.blade.php
+++ resources/views/livewire/events/show/actions.blade.php
@@ -1,19 +1,53 @@
 <div class="event__status">
     <span class="event__status__title" for="timer">Durée&nbsp;:</span>
 
-    @if ($event->started_at !== null)
-        <span id="timer" wire:poll.1s="updateTimer">
+    {{-- Event timer --}}
+    @if ($event->started_at !== null && $event->paused_at === null && $event->finished_at === null)
+        <span class="event__status__timer" id="timer" wire:poll.1s="updateTimer">
             {{ $hours }}:{{ $minutes }}:{{ $seconds }}
         </span>
-    @else
-        <span id="timer">00:00:00</span>
+    @elseif ($event->paused_at !== null && $event->finished_at === null)
+        <span class="event__status__timer-pause" id="timer">
+            EN PAUSE
+        </span>
+    @elseif ($event->finished_at === null)
+        <span class="event__status__timer" id="timer" wire:poll.1s="updateTimer">
+            {{ $hours }}:{{ $minutes }}:{{ $seconds }}
+        </span>
     @endif
 
-    <label for="status">Status&nbsp;:</label>
-    <select id="status" wire:change="updateStatus($event.target.value)">
-        <option disabled>Choisir un status</option>
-        <option value="started" selected>En cours</option>
-        <option value="paused">En pause</option>
-        <option value="finished">Terminer</option>
-    </select>
+    {{-- Event status --}}
+    <span class="event__status__title">Status&nbsp;:</span>
+    <span class="event__status__actions">
+        @if ($event->started_at !== null && $event->paused_at === null && $event->finished_at === null)
+            <span class="event__status__actions__started">En cours</span>
+            <span class="event__status__actions__paused">
+                <button class="button--white" type="button" wire:click="pauseEvent">
+                    <x-svg.pause />
+                </button>
+            </span>
+        @elseif ($event->paused_at !== null && $event->finished_at === null)
+            <span class="event__status__actions__finished">En pause</span>
+        @elseif ($event->finished_at !== null)
+            <span class="event__status__actions__finished">Terminé</span>
+        @else
+            En attente
+        @endif
+    </span>
+
+    @if($event->paused_at !== null && $event->finished_at === null)
+        <x-dialog wire:model="eventOnPause">
+            <x-dialog.panel>
+                <div class="form__content">
+                    <h2 class="title">Modification du status</h2>
+                    <p>L'épreuve à été mise en pause. Voulez-vous la reprendre ou plutôt la terminée définitevement&nbsp;?</p>
+                </div>
+
+                <x-dialog.footer>
+                    <button type="button" wire:click="finishEvent" class="cancel">Terminer</button>
+                    <button type="button" wire:click="continueEvent" class="save">Continuer</button>
+                </x-dialog.footer>
+            </x-dialog.panel>
+        </x-dialog>
+    @endif
 </div>
